24df33389b99d28ac8b3d465ebe492c064efd87e
F.Red <fredgsfm@gmail.com>
2026-06-03 14:37:28 +0100

    Delete backend/app/security directory

--- backend/app/security/data_segmentation.py
+++
@@ -1,9 +0,0 @@
-
-class DataSegmentation:
-
-    def segregate(self, process_data):
-
-        return {
-            "public_view": {},
-            "institutional_view": process_data
-        }
--- backend/app/security/session_guard.py
+++
@@ -1,8 +0,0 @@
-
-class SessionGuard:
-
-    MAX_REQUESTS_PER_MINUTE = 60
-
-    def validate_rate_limit(self, requests):
-
-        return requests < self.MAX_REQUESTS_PER_MINUTE
