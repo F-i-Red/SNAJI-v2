4f03e90ba89278b2e707f66090bed3e703638cc2
F.Red <fredgsfm@gmail.com>
2026-06-03 14:58:14 +0100

    Delete backend/app/core/config.py

--- backend/app/core/config.py
+++
@@ -1,9 +0,0 @@
-
-from pydantic_settings import BaseSettings
-
-class Settings(BaseSettings):
-    APP_NAME: str = "SNAJI"
-    DEBUG: bool = True
-    DATABASE_URL: str = "postgresql://snaji:snaji@db:5432/snaji"
-
-settings = Settings()
